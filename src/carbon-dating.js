e00f848fa11c68a89ae7c9cebd31a59a833e0f7c
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-04 23:18:12 +0100

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !=='string' || 
+      isNaN(sampleActivity) || 
+      Number(sampleActivity) <= 0 || 
+      Number(sampleActivity) > 15) {
+    return false
+  } else {
+    let k = 0.693 / 5730;
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k)
+  }
 }
 
 module.exports = {
