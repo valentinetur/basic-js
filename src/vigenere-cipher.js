45aa7f9f83c84c8cffeb1fac4f3c302f32939246
Valentine Tur <valentine.tur.it@gmail.com>
2025-01-06 22:09:35 +0100

    feat: add vigenere-cipher decision

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    return this.process(text, key, true);
+  }
+
+  decrypt(text, key) {
+    return this.process(text, key, false);
+  }
+
+  process(text, key, encrypt) {
+    if (!text || !key) throw new Error("Incorrect arguments!");
+
+    text = text.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = "";
+    let keyIndex = 0;
+    const A = 65;
+
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+
+      if (char >= 'A' && char <= 'Z') {
+        const shift = key[keyIndex % key.length].charCodeAt(0) - A;
+        keyIndex++;
+
+        const newChar = encrypt
+          ? String.fromCharCode(((char.charCodeAt(0) - A + shift) % 26) + A)
+          : String.fromCharCode(((char.charCodeAt(0) - A - shift + 26) % 26) + A);
+
+        result += newChar;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.direct ? result : result.split("").reverse().join("");
   }
 }
+
+module.exports = {
+  VigenereCipheringMachine
+};
+
 
 module.exports = {
   VigenereCipheringMachine
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0) {
+    throw new Error("Invalid date!");
+  } 
+
+  const month = date.getMonth();
+
+  
+
+  if ([11, 0, 1].includes(month)) return "winter";
+  if ([2, 3, 4].includes(month)) return "spring";
+  if ([5, 6, 7].includes(month)) return "summer";
+  if ([8, 9, 10].includes(month)) return "autumn";
 }
 
 module.exports = {
